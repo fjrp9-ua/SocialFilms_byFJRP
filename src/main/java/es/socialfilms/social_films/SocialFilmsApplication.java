2d385e625e41a869f07f8757e524ff5b46bf5156
Francisco José Ramirez Paraíso <fjrp9@mscloud.ua.es>
2024-10-24 19:18:15 +0200

    Initia version

---
+++ src/main/java/es/socialfilms/social_films/SocialFilmsApplication.java
@@ -0,0 +1,13 @@
+package es.socialfilms.social_films;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFilmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/socialfilms/social_films/SocialFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.socialfilms.social_films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
