da5f97ee337f3d9f8aff4d5738343c0dab48748a
Francisco José Ramirez Paraíso <fjrp9@mscloud.ua.es>
2024-11-01 13:57:46 +0100

    Add new service class: FilmService

---
+++ src/main/java/es/socialfilms/social_films/service/FilmService.java
@@ -0,0 +1,32 @@
+package es.socialfilms.social_films.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.socialfilms.social_films.dto.FilmData;
+import es.socialfilms.social_films.repository.FilmRepository;
+
+@Service
+public class FilmService {
+
+    @Autowired
+    private FilmRepository filmRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Transactional(readOnly = true)
+    public List<FilmData> getAllFilms(){
+        
+        List<FilmData> films = filmRepository.findAllByOrderById().stream()
+            .map(film -> modelMapper.map(film, FilmData.class))
+            .collect(Collectors.toList());
+
+        return films;
+    }
+}
