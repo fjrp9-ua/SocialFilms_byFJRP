b4e322f30542491f32e27cc3dc5d44ce31cdea26
Francisco José Ramirez Paraíso <fjrp9@mscloud.ua.es>
2024-11-04 18:22:16 +0100

    New Model Class: Review

---
+++ src/main/java/es/socialfilms/social_films/model/Review.java
@@ -0,0 +1,75 @@
+package es.socialfilms.social_films.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
+public class Review implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String review;
+
+    private float rating;
+
+    // Constructors ...
+
+    public Review(){}
+
+    public Review(String review, float rating){
+        this.review = review;
+        this.rating = rating;
+    }
+
+    // Getters and Setters ...
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getReview(){
+        return review;
+    }
+
+    public void setReview(String review){
+        this.review = review;
+    }
+
+    public float getRating(){
+        return rating;
+    }
+
+    public void setRating(float rating){
+        this.rating = rating;
+    }
+
+    // Overrides ...
+
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+
+        Review review = (Review) o;
+        if (this.id != null && review.id != null) return Objects.equals(this.getId(), review.getId());
+        return getReview().equals(review.getReview()) && getRating() == review.getRating();
+    }
+
+    public int hashCode(){
+        return Objects.hash(review, rating);
+    }
+}
