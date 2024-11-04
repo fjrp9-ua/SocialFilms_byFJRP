9aed113d5e4e8db9745b655c0b557a02c6b0d211
Francisco José Ramirez Paraíso <fjrp9@mscloud.ua.es>
2024-11-04 18:07:56 +0100

    New Controller: UserController

---
+++ src/main/java/es/socialfilms/social_films/controller/UserController.java
@@ -0,0 +1,26 @@
+package es.socialfilms.social_films.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import es.socialfilms.social_films.dto.UserData;
+import es.socialfilms.social_films.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user/{id}")
+    @ResponseBody
+    public UserData getUserById(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+    
+}
